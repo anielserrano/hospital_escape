93d63aecbea15e082e5a3816675620735e70127e
anielserrano <anielserrano112@gmail.com>
2026-09-23 01:33:30 -0500

    New updated one - Aniel

--- hospital_escape.c
+++ hospital_escape.c
@@ -41,85 +41,85 @@ int main(void) {
             printf("You put the keycard in your pocket and enter the hallway.\n");
         
             items[0] = 1;
-    }
+        }
     
-    else if  (choice == 2) {
+        else if  (choice == 2) {
         
-        printf("You leave the room and enter the hallway.\n");
-    }
+            printf("You leave the room and enter the hallway.\n");
+        }
     
-    else {
+        else {
         
-        printf("Invalid choice.\n\n");
-    }
-}    
+            printf("Invalid choice.\n\n");
+        }
+    }    
 
-while (gameOver == 0) {
+    while (gameOver == 0) {
         
-    printf("\n====================================\n");
-    printf("            MAIN HALLWAY\n");
-    printf("====================================\n");
+        printf("\n====================================\n");
+        printf("            MAIN HALLWAY\n");
+        printf("====================================\n");
         
-    printf("\nYou step into the hallway.\n");
-    printf("The hallway splits into three directions.\n");
-    printf("To your left, you see a sign for the Pharmacy Room.\n");
-    printf("Straight ahead is the Operating Room.\n");
-    printf("To your right is a door that says Security Room.\n");
-    printf("Or go back to the Hospital Room.\n\n");
+        printf("\nYou step into the hallway.\n");
+        printf("The hallway splits into three directions.\n");
+        printf("To your left, you see a sign for the Pharmacy Room.\n");
+        printf("Straight ahead is the Operating Room.\n");
+        printf("To your right is a door that says Security Room.\n");
+        printf("Or go back to the Hospital Room.\n\n");
         
-    printf("\n------------------------------------\n");
-    printf("Which direction do you want to take?\n");
-    printf("1. Go left to the Pharmacy Room.\n");
-    printf("2. Go straight toward the Operating Room.\n");
-    printf("3. Go right to the Security Room.\n");
-    printf("4. Go back to the Hospital Room.\n");
-    printf("Enter your choice: ");
+        printf("\n------------------------------------\n");
+        printf("Which direction do you want to take?\n");
+        printf("1. Go left to the Pharmacy Room.\n");
+        printf("2. Go straight toward the Operating Room.\n");
+        printf("3. Go right to the Security Room.\n");
+        printf("4. Go back to the Hospital Room.\n");
+        printf("Enter your choice: ");
 
     
-    scanf("%d", &choice);
-    printf("\n"); 
+        scanf("%d", &choice);
+        printf("\n"); 
          
         
-    if (choice == 1) {
-        printf("\n====================================\n");
-        printf("            PHARMACY ROOM\n");
-        printf("====================================\n");
+        if (choice == 1) {
+            printf("\n====================================\n");
+            printf("            PHARMACY ROOM\n");
+            printf("====================================\n");
 
-        if (items[1] == 0) {
+            if (items[1] == 0) {
             
-            printf("You search around the room and found a flashlight!\n");
+                printf("You search around the room and found a flashlight!\n");
             
-            items[1] = 1;
-        }
+                items[1] = 1;
+            }
         
-        else {
+            else {
             
-            printf("You already searched this room and took the flashlight.\n");
+                printf("You already searched this room and took the flashlight.\n");
+            }
+
+            printf("You return back to the main hallway.\n");
         }
 
-        printf("You return back to the main hallway.\n");
-    }
-
-    else if (choice == 4) {
-        printf("\n====================================\n");
-        printf("            HOSPITAL ROOM\n");
-        printf("====================================\n");
+        else if (choice == 4) {
+            printf("\n====================================\n");
+            printf("            HOSPITAL ROOM\n");
+            printf("====================================\n");
         
-        if (items[0] == 0) {
+            if (items[0] == 0) {
             
-            printf("You search around the room and found the keycard.\n");
+                printf("You search around the room and found the keycard.\n");
            
-            items[0] = 1;
-        }
-        else {
+                items[0] = 1;
+            }
+            else {
             
-            printf("You already searched this room and took the keycard.\n");
-        }
+                printf("You already searched this room and took the keycard.\n");
+            }
         
-        printf("You return back to the main hallway.\n");
+            printf("You return back to the main hallway.\n");
+        }
     }
-}
 
-return 0;
+    return 0;
 
 }           
